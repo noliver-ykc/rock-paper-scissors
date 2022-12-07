b58abf75553d69849354cdae9f38b35e7d1d7b0b
noliver-ykc <noliver.ykc@gmail.com>
2022-12-07 05:49:24 +0000

    start button triggers shake animation

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
+// choose a random weapon for cpu
+const options = ["🪨","📃","✂️"];
+
 function rockPaperScissorsCPU(options) {
   let choice = options[Math.floor(Math.random()*options.length)];
   return choice;
 }
 
-const options = ["🪨","📃","✂️"];
 
 
-const btn = document.querySelector('#setWeapon');        
+// user weapon choice    
 const radioButtons = document.querySelectorAll('input[name="radio-group"]');
 const output = document.querySelector('.user-fist')
 for (const radioButton of radioButtons) {
@@ -17,6 +19,22 @@ for (const radioButton of radioButtons) {
     }
     // show the output:
     output.innerText = selection;
-    console.log("here");
   });
 }
+
+
+// game start btn causes btns to shake
+let startBtn = document.querySelector('#gameStart');
+const user = document.querySelector('.user-fist');
+const cpu = document.querySelector('.cpu-fist');
+startBtn.addEventListener("click", () => {
+  user.classList.add("shake");
+  cpu.classList.add("shake");
+});
+
+// game start btn causes btns to shake
+let newGamebtn = document.querySelector('#newGame');
+newGamebtn.addEventListener("click", () => {
+  user.classList.remove("shake");
+  cpu.classList.remove("shake");
+});
