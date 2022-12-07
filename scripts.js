734a0a2f710f35dc84f52127b6b1562c475da895
noliver-ykc <noliver.ykc@gmail.com>
2022-12-07 06:34:41 +0000

    pressing start triggers CPU choice

--- scripts.js
+++ scripts.js
@@ -1,12 +1,3 @@
-// choose a random weapon for cpu
-const options = ["🪨","📃","✂️"];
-
-function rockPaperScissorsCPU(options) {
-  let choice = options[Math.floor(Math.random()*options.length)];
-  return choice;
-}
-
-
 
 // user weapon choice    
 const radioButtons = document.querySelectorAll('input[name="radio-group"]');
@@ -27,14 +18,27 @@ for (const radioButton of radioButtons) {
 let startBtn = document.querySelector('#gameStart');
 const user = document.querySelector('.user-fist');
 const cpu = document.querySelector('.cpu-fist');
+const options = ["🪨","📃","✂️"];
 startBtn.addEventListener("click", () => {
   user.classList.add("shake");
   cpu.classList.add("shake");
+  let cpuChoice = rockPaperScissorsCPU(options);
+  cpu.innerText = cpuChoice;
+  startBtn.disabled = true;
 });
 
 // game start btn causes btns to shake
 let newGamebtn = document.querySelector('#newGame');
 newGamebtn.addEventListener("click", () => {
   user.classList.remove("shake");
+  user.innerText = "✊";
+  cpu.innerText = "✊";
   cpu.classList.remove("shake");
+  startBtn.disabled = false;
 });
+
+// choose a random weapon for cpu
+function rockPaperScissorsCPU(options) {
+  let choice = options[Math.floor(Math.random()*options.length)];
+  return choice;
+}
